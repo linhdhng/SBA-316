fa08bdba81861b7d9466c23c4becf5539d30d859
linhdhng <153128184+linhdhng@users.noreply.github.com>
2024-02-01 23:36:32 -0600

    Update j.js

--- SBA 316/j.js
+++ SBA 316/j.js
@@ -14,6 +14,22 @@
 // Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
 // Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
 
+// Cache at least one element using selectElementById.
+// Cache at least one element using querySelector or querySelectorAll.
+// Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.)
+// Iterate over a collection of elements to accomplish some task.
+//Create at least one element using createElement.
+// Use appendChild and/or prepend to add new elements to the DOM.
+// Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 
+// Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+// Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+// Modify at least one attribute of an element in response to user interaction.
+// Register at least two different event listeners and create the associated event handler functions.
+// Use at least two Browser Object Model (BOM) properties or methods.
+// Include at least one form and/or input with HTML attribute validation.
+// Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
+// Ensure that the program runs without errors (comment out things that do not work, and explain your blockers - you can still receive partial credit).
+
 function cardCarousel3d(carousels){
     var rotateHandler = function(evt) {
       var carousel = this.parentElement;
@@ -30,24 +46,20 @@ function cardCarousel3d(carousels){
       animate_slider(carousel);
     }
     for(var i = 0; i < carousels.length; i++) {
-      let carousel = carousels[i];
-      let inner = carousel.querySelector('.inner-carousel');
-      let cards = carousel.querySelectorAll('.inner-carousel > div');
-      var size = cards.length;
-      var btnLeft = carousel.querySelector('.button-spin.counterclockwise');
-      if(btnLeft !== null) {
-        btnLeft.addEventListener("click", rotateHandler, false);
-      }
-      var btnRight = carousel.querySelector('.button-spin.clockwise');
-      if(btnRight !== null) {
-        btnRight.addEventListener("click", rotateHandler, false);  
-      }
-      function onPictureShown(pictureId) {
-        if (pictureId === "123") {
-            alert("Picture with id 123 is shown!");
+        let carousel = carousels[i];
+        let inner = carousel.querySelector('.inner-carousel');
+        let cards = carousel.querySelectorAll('.inner-carousel > div');
+        var size = cards.length;
+        var btnLeft = carousel.querySelector('.button-spin.counterclockwise');
+        if(btnLeft !== null) {
+            btnLeft.addEventListener("click", rotateHandler, false);
         }
-    }
-      animate_slider(carousel);
+        var btnRight = carousel.querySelector('.button-spin.clockwise');
+        if(btnRight !== null) {
+            btnRight.addEventListener("click", rotateHandler, false);  
+        }
+        
+        animate_slider(carousel);
     }
     
     function animate_slider(carousel){
@@ -69,8 +81,24 @@ function cardCarousel3d(carousels){
         child.classList.remove('front');
         child.classList.remove('counterclockwise');
         child.removeEventListener("click", rotateHandler, false);
-      }
+        var zz = z % 360;
+            if(zz === 0) {
+            child.classList.add('front');
+            } else if (zz === ry || zz === -360 + ry) {
+                child.classList.add('clockwise');
+                child.addEventListener("click", function() {
+                    if (this.id === 'signature') {
+                      alert('You got the GOLDEN CARD!');
+                    }
+                    rotateHandler.call(this); // Call rotateHandler after the alert
+                    }, false);
+            } else if (zz === 360 - ry || zz === 0 - ry) {
+                child.classList.add('counterclockwise');
+                child.addEventListener("click", rotateHandler, false);
+            }
+        }
     }
-  }
+}
   
   cardCarousel3d(document.querySelectorAll('.card-carousel'));
+
